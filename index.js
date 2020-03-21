e435495a90eaab769360a837b81f1e2e4d5b0b0d
Tyler Butler <tyler@tylerbutler.com>
2020-03-21 11:47:18 -0700

    fixes

--- index.js
+++ index.js
@@ -3,23 +3,24 @@ const path = require('path');
 const process = require('process');
 const template = require('lodash.template');
 
-module.exports = (obj) => function TEMPLATE(content, options = {}, config) {
+module.exports = (dataObject) => function TEMPLATE(content, options = {}, config) {
   // any options passed in with values of the form $variable will be expanded as
   // environment variables
   for (let [key, value] of Object.entries(options)) {
     if (value.startsWith('$')) {
       const envVar = value.substring(1);
       const envVarVal = process.env[envVar];
+      console.log(`${envVar} set to ${envVarVal}`);
       options[key] = envVarVal;
     }
   }
 
-  // obj.vars = vars;
+  options.VARS = dataObject;
   let templateString = content;
   if (options.src) {
     const filepath = path.resolve(path.dirname(config.originalPath), options.src);
     templateString = fs.readFileSync(filepath, 'utf8');
   }
 
-  return template(templateString)(obj);
+  return template(templateString)(options);
 }
